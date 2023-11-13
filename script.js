6ba6fb796b528647f0507b4e0a60a0a60b74e814
moonleeks <moonleeks@proton.me>
2023-11-13 14:22:07 +0200

    finished getComputerChoice

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function getComputerChoice() {
-    let result;
-    result = "rock";
-    return result;
+    let result = Math.floor(Math.random() * 3);
+    switch (result) {
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
