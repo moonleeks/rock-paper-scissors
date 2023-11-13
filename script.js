cc28104df9cf3a16c5183595f02f62cd903380cd
moonleeks <moonleeks@proton.me>
2023-11-13 14:15:39 +0200

    finished playRound()

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Hello");
+function getComputerChoice() {
+    let result;
+    result = "rock";
+    return result;
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelection = playerSelection.toLower();
+
+    if (playerSelection === computerSelection) {
+        return "Tied";
+    }
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You win! Scissors beats Rock";
+        } else {
+            return "Error";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats paper";
+        } else if (computerSelection === "rock") {
+            return "You win! Paper beats rock";
+        } else {
+            return "Error";
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats scissors";
+        } else if (computerSelection = "paper") {
+            return "You win! Scissors beats paper"
+        } else {
+            return "Error";
+        }
+    }
+}
+
+console.log(getComputerChoice());
+
